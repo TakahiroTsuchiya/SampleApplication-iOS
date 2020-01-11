e64e7cb2bdced2554d4e4d072751182ce2a8d6e1
Takahiro Tsuchiya <sample@sample.com>
2020-01-11 21:31:37 +0900

    add new project

---
+++ SampleApplication-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleApplication-iOS
+//
+//  Created by Takahiro Tsuchiya on 2020/01/11.
+//  Copyright © 2020 Takahiro Tsuchiya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
