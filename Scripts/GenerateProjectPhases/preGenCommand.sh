cdfdd4bf13f8b5f52fef83a1c17dfd047cb90028
Takahiro Tsuchiya <sample@sample.com>
2020-05-18 20:45:18 +0900

    add Firebase Crashlytics settings

--- Scripts/GenerateProjectPhases/preGenCommand.sh
+++ Scripts/GenerateProjectPhases/preGenCommand.sh
@@ -7,7 +7,16 @@ echo "------------------------------------------------------"
 echo "--- Carthage update --platform ios"
 carthage update --platform ios
 
+echo "--- Firebase Crashlytics upload-symbols　Download "
+curl -L https://github.com/firebase/firebase-ios-sdk/raw/master/Crashlytics/upload-symbols -o Scripts/Firebase/Crashlytics/upload-symbols
+
+echo "--- Firebase Crashlytics run　Download "
+curl -L https://github.com/firebase/firebase-ios-sdk/raw/master/Crashlytics/run -o Scripts/Firebase/Crashlytics/run
+
+echo "--- add execute "
+chmod 755 Scripts/Firebase/Crashlytics/*
+
+
 echo "------------------------------------------------------"
 echo "--- pre END"
 echo "------------------------------------------------------"
-
